4a8ba31599802731913a0b906930287761e24c7e
Jesús Romero <46077173+jeromflo@users.noreply.github.com>
2023-04-16 16:58:36 +0200

    primer commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const users = [
+  {id: 1, nombre: "Alejandro"},
+  {id: 2, nombre: "Jesus"},
+  {id: 3, nombre: "Juan"},
+];
+app.get("/users", (req, res) => {
+  res.status(200).json(users);
+});
+
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+  const user = users.find(user => user.id === parseInt(id));
+  res.status(200).json(user);
+});
+
+exports.users = app;
+app.listen(3000, () => console.log("Listening on port 3000!"));
